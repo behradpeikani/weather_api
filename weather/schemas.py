8110d8e837125f8ab6b75b6f0880dd42dc803ee8
behradpeikani <behradpeikani@gmail.com>
2023-11-16 20:15:54 +0330

    feat(weather/schemas.py): I added WeatherOutSchema and renamed WeatherSchema to WeatherInSchema

--- weather/schemas.py
+++ weather/schemas.py
@@ -1,6 +1,16 @@
 from ninja import Schema, Field
-    
 
-class WeatherSchema(Schema):
+
+class WeatherInSchema(Schema):
     city: str = Field(..., max_length=20)
-    country: str = Field(..., max_length=2)
+    country: str = Field(..., max_length=20)
+
+
+class WeatherOutSchema(Schema):
+    min_temperature: str
+    max_temperature: str
+    wind_speed: str
+    pressure: str
+    humidity: str
+    coord_lat: str
+    coord_lon: str
