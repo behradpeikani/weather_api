90ffd801890bc3429e1bbb7380d418c28a77d0f9
behradpeikani <behradpeikani@gmail.com>
2023-11-16 20:40:20 +0330

    fix(weather/utils.py): I fixed some bugs in weather_dict

--- weather/utils.py
+++ weather/utils.py
@@ -5,39 +5,33 @@ import json
 
 def get_weather_data(city, country):
 	api_key = settings.WEATHER_API_KEY
-	url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}'
+	url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
 
 	response = requests.get(url)
 
-	# current_json = json.loads(response)
-	current_json = response.json()
-	weather_data = current_data['current']
+	if response.status_code == 200:
+		weather_data = response.json()
 
-	location_name = f"{weather_data['city']['@name']}, {weather_data['city']['country']}"
-	temperature = float(weather_data['temperature']['@value'])
-	wind_speed = weather_data['wind']['speed']                              
-	wind_dir = weather_data['wind']['direction']                          
-	cloudiness = weather_data['clouds']['@name']                       
-	pressure = weather_data['pressure']['@value']                         
-	humidity = weather_data['humidity']['@value']                                  
+	temp_min = weather_data['main']['temp_min']
+	temp_max = weather_data['main']['temp_max']
+	wind_speed = weather_data['wind']['speed']                                                                               
+	pressure = weather_data['main']['pressure']                         
+	humidity = weather_data['main']['humidity']                                  
 	
 	# Geographical coordinates 
-	coord_lat = weather_data['city']['coord']['@lat']                                   
-	coord_lon = weather_data['city']['coord']['@lon']                                            
-  
-	requested_time = weather_data['lastupdate']['@value']                  
+	coord_lat = weather_data['coord']['lat']                                   
+	coord_lon = weather_data['coord']['lon']                                                             
 	
 
 	# Final formatted data dictionary   
 	weather_dict = {
-	'location_name': location_name,
-	'temperature': f'{temperature}°C',
-	'wind': f'{wind_speed} m/s, {wind_dir}',
-	'cloudiness': cloudiness,
-	'pressure': f'{pressure} hpa',
+	'min_temperature': f'{temp_min}°C',
+	'max_temperature': f'{temp_max}°C',
+	'wind_speed': f'{wind_speed} m/s',
+	'pressure': f'{pressure} hPa',
 	'humidity': f'{humidity} %',
-	'geo_coordinates': f'[{coord_lat}, {coord_lon}]',
-	'requested_time': requested_time
+	'coord_lat': f'{coord_lat}',
+	'coord_lon': f'{coord_lon}',
 }
 
 
