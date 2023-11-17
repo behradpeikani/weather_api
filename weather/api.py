ebb4727b74e83a3ed03b01fb2a5f1cbdc15f1c55
behradpeikani <behradpeikani@gmail.com>
2023-11-17 20:57:18 +0330

    feat(weather/api.py): I added exeption and permission that only authorized users can access to this API

--- weather/api.py
+++ weather/api.py
@@ -1,10 +1,12 @@
 from ninja import Router
 from .schemas import WeatherInSchema, WeatherOutSchema
 from .utils import get_weather_data  
+from ninja.errors import HttpError
+from ninja.security import django_auth
 
 weather_router = Router()
 
-@weather_router.post("/weather", response=WeatherOutSchema)
+@weather_router.post("/weather", response={200: WeatherOutSchema}, auth=django_auth)
 def post_weather(request, payload: WeatherInSchema):
     """
     Retrieve weather data based on provided city and country.
@@ -12,18 +14,23 @@ def post_weather(request, payload: WeatherInSchema):
     city = payload.city
     country = payload.country
 
-    # Fetch weather data using the provided city and country
-    weather_data = get_weather_data(city, country)
 
-    # Format the fetched weather data into WeatherOutSchema
-    formatted_weather_data = WeatherOutSchema(
-        min_temperature=weather_data['min_temperature'],
-        max_temperature=weather_data['max_temperature'],
-        wind_speed=weather_data['wind_speed'],
-        pressure=weather_data['pressure'],
-        humidity=weather_data['humidity'],
-        coord_lat=weather_data['coord_lat'],
-        coord_lon=weather_data['coord_lon']
-    )
+    try:
+        # Fetch weather data using the provided city and country
+        weather_data = get_weather_data(city, country)
 
-    return formatted_weather_data
+        # Format the fetched weather data into WeatherOutSchema
+        formatted_weather_data = WeatherOutSchema(
+            min_temperature=weather_data['min_temperature'],
+            max_temperature=weather_data['max_temperature'],
+            wind_speed=weather_data['wind_speed'],
+            pressure=weather_data['pressure'],
+            humidity=weather_data['humidity'],
+            coord_lat=weather_data['coord_lat'],
+            coord_lon=weather_data['coord_lon']
+        )
+
+        return formatted_weather_data
+
+    except Exception:
+        raise HttpError(404, 'The desired city or country was not found!')
